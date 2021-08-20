cdfb9f350dcd1f7d686642ba4b7136709a7e00d3
Ryan <ryanmahara@gmail.com>
2021-08-20 00:00:46 -0500

    added success and error animation funcationality to email input

--- script.js
+++ script.js
@@ -5,45 +5,76 @@ const emailFieldEl = document.querySelector('.email-container input[type=email]'
 const emailSubmitBtn = document.querySelector('.email-container button');
 
 const emailErrorElCreation = function (e) {
+  //guard clause - stops from over generating
+  if (document.querySelector('.errMsg')) return;
+
   // parent element
   const parentContainer = e.target.parentElement;
 
   //content
   const emailError = document.createElement('p');
   emailError.textContent = 'Oops! Please check your email';
-
-  //standard css
-  emailError.style.position = 'absolute';
-  emailError.style.color = '#fb3e3e';
-  emailError.style.fontFamily = 'var(--ff-primary)';
-  emailError.style.fontSize = '0.75rem';
-  emailError.style.lineHeight = '0.75rem';
-  emailError.style.fontWeight = '700';
-  emailError.style.left = '29px';
-
-  // dynamic css based on parent
-  const parentContainerHt = window.getComputedStyle(parentContainer).height;
-  emailError.style.top = Number.parseFloat(parentContainerHt) + 8 + 'px';
-
-  // attach css to parent
-  parentContainer.style.position = 'relative';
+  emailError.classList.add('errMsg');
 
   // insert html
   parentContainer.insertAdjacentElement('beforeend', emailError);
 };
 
-emailSubmitBtn.addEventListener('click', function (e) {
+const addShakeClass = function () {
+  // remove incase of screen resize and to replay animation
+  emailFieldEl.classList.remove('err-animate');
+  emailSubmitBtn.classList.remove('err-animate');
+
+  // 640px is when the email form grid separates
+  if (window.innerWidth <= 640) {
+    emailFieldEl.classList.add('err-animate');
+  } else {
+    emailFieldEl.classList.add('err-animate');
+    emailSubmitBtn.classList.add('err-animate');
+  }
+};
+
+const addSpinClass = function () {
+  // remove incase of screen resize and to replay animation
+  emailFieldEl.classList.remove('success');
+  emailSubmitBtn.classList.remove('success');
+
+  // 640px is when the email form grid separates
+  if (window.innerWidth <= 640) {
+    emailFieldEl.classList.add('success');
+  } else {
+    emailFieldEl.classList.add('success');
+    emailSubmitBtn.classList.add('success');
+  }
+};
+
+const emailFormChecker = function (e) {
   const userInput = emailFieldEl.value;
 
   // prettier-ignore
   const emailRefExp = new RegExp(/^\w+([\.-]?\w+)@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
 
   if (userInput.match(emailRefExp)) {
-    console.log('pass');
+    setTimeout(() => (emailFieldEl.value = ''), 500);
+    addSpinClass();
   } else {
-    // console.log(emailError);
+    addShakeClass();
     emailErrorElCreation(e);
   }
+};
+
+// event listeners
+emailSubmitBtn.addEventListener('click', emailFormChecker);
+
+emailContainer.addEventListener('keydown', function (e) {
+  if (e.key !== 'Enter') return;
+  emailFormChecker(e);
 });
 
-//   https://www.w3resource.com/javascript/form/email-validation.php
+// reset error message when user begins typing again
+emailFieldEl.addEventListener('input', function (e) {
+  //
+  if (!document.querySelector('.errMsg')) return;
+
+  document.querySelector('.errMsg').remove();
+});
